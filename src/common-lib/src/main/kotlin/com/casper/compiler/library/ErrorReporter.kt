f8da80bd04da595f2a2ff000c084f131e7fb6245
Casper <133casper@gmail.com>
2020-12-02 23:30:28 +0300

    Add lexer functionality

--- src/common-lib/src/main/kotlin/com/casper/compiler/library/ErrorReporter.kt
+++ src/common-lib/src/main/kotlin/com/casper/compiler/library/ErrorReporter.kt
@@ -3,6 +3,6 @@ package com.casper.compiler.library
 var hadError = false
 
 fun reportError(line: Int, message: String) {
-    System.err.println("[Line $line] Error: $message")
+    System.err.println("[Line $line] Error: $message.")
     hadError = true
 }
